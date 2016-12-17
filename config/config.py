eb8c7f142d096d7d4f5682cc02304b24b2b54d6b
Warframe <thatsacoolbike@gmail.com>
2016-12-17 10:33:35 -0800

    Update config.py

--- config/config.py
+++ config/config.py
@@ -2,6 +2,6 @@ config = {
 	'server': 'irc.twitch.tv',
 	'port': 6667,
 	'username': 'ib00t',
-	'password': 'oauth:g3l40y9hq04m4mj0520p5bscgkad98',
+	'password': 'oauth:0y9hq04m4mj0520p5bscgkad98',
 	'channels': ['garge1', 'doomclanlive', 'taaaaasty', 'coldsidetv', 'twitchtvmaverick', 'ghostninjatv']
 }
